5bbd52ced45bde30a9714b23809f6c1e33b5c938
Vladimir <vladimir97927@gmail.com>
2025-04-20 16:03:53 +0700

    Add FiletVersionManager doc

--- src/SnkUpdateMaster.Core/VersionManager/FileVersionManager.cs
+++ src/SnkUpdateMaster.Core/VersionManager/FileVersionManager.cs
@@ -1,16 +1,48 @@
 ﻿
 namespace SnkUpdateMaster.Core.VersionManager
 {
+    /// <summary>
+    /// Реализация менеджера версий, использующая файловую систему для хранения информации
+    /// </summary>
+    /// <remarks>
+    /// Класс обеспечивает:
+    /// <list type="bullet">
+    /// <item><description>Хранение версии в текстовом файле формата "major.minor.build"</description></item>
+    /// <item><description>Автоматическое создание файла версии при первом обновлении</description></item>
+    /// </list>
+    /// 
+    /// Пример файла version:
+    /// <code>1.2.3</code>
+    /// 
+    /// <para>Расположение файла: {текущая директория приложения}/version</para>
+    /// </remarks>
     public class FileVersionManager : ICurrentVersionManager
     {
         private readonly string _versionFileName = "version";
 
+        /// <summary>
+        /// Получает текущую версию из файла
+        /// </summary>
+        /// <returns>
+        /// <para>Version - объект версии при успешном чтении и парсинге</para>
+        /// <para>null - в случаях:</para>
+        /// <list type="bullet">
+        /// <item><description>Файл версии не существует</description></item>
+        /// <item><description>Неверный формат данных в файле</description></item>
+        /// <item><description>Ошибка доступа к файлу</description></item>
+        /// </list>
+        /// </returns>
+        /// <exception cref="UnauthorizedAccessException">
+        /// Нет прав на чтение файла
+        /// </exception>
+        /// <exception cref="IOException">
+        /// Ошибка ввода-вывода при работе с файлом
+        /// </exception>
         public async Task<Version?> GetCurrentVersionAsync()
         {
             var versionFilePath = Path.Combine(Environment.CurrentDirectory, _versionFileName);
             if (!File.Exists(versionFilePath))
             {
-                // throw new FileNotFoundException("Файл версии не найден", versionFilePath);
                 return null;
             }
 
@@ -20,10 +52,26 @@ namespace SnkUpdateMaster.Core.VersionManager
                 return version;
             }
 
-            //throw new InvalidOperationException("Неверный формат версии в файле");
             return null;
         }
 
+        /// <summary>
+        /// Обновляет версию в файле
+        /// </summary>
+        /// <param name="version">Объект новой версии</param>
+        /// <exception cref="IOException">
+        /// Ошибка записи в файл версии
+        /// </exception>
+        /// <exception cref="UnauthorizedAccessException">
+        /// Нет прав на запись в файл
+        /// </exception>
+        /// <remarks>
+        /// Особенности реализации:
+        /// <list type="bullet">
+        /// <item><description>Создает файл версии при первом вызове</description></item>
+        /// <item><description>Полностью перезаписывает содержимое файла</description></item>
+        /// </list>
+        /// </remarks>
         public async Task UpdateCurrentVersionAsync(Version version)
         {
             var versionFilePath = Path.Combine(Environment.CurrentDirectory, _versionFileName);
