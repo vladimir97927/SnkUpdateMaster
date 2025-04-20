298bd4c9aa222c4d2f67892913b249cb85d5ad43
Vladimir <vladimir97927@gmail.com>
2025-04-20 16:18:36 +0700

    Add ZipInstaller doc

--- src/SnkUpdateMaster.Core/Installer/ZipInstaller.cs
+++ src/SnkUpdateMaster.Core/Installer/ZipInstaller.cs
@@ -3,18 +3,53 @@ using System.IO.Compression;
 
 namespace SnkUpdateMaster.Core.Installer
 {
+    /// <summary>
+    /// Реализация установщика обновлений из ZIP-архива
+    /// </summary>
+    /// <remarks>
+    /// Обеспечивает установку через:
+    /// <list type="number">
+    /// <item><description>Создание полной резервной копии приложения</description></item>
+    /// <item><description>Распаковку архива поверх текущей версии</description></item>
+    /// <item><description>Автоматический откат при ошибках установки</description></item>
+    /// </list>
+    /// 
+    /// Особенности реализации:
+    /// <list type="bullet">
+    /// <item><description>Резервная копия сохраняется в поддиректорию /backup</description></item>
+    /// </list>
+    /// </remarks>
     public class ZipInstaller : IInstaller
     {
         private readonly string _appDir;
 
         private readonly string _backupDir;
 
+        /// <summary>
+        /// Инициализирует новый экземпляр установщика
+        /// </summary>
+        /// <param name="appDir">Корневая директория приложения для обновления</param>
+        /// <exception cref="ArgumentException">
+        /// Возникает при недопустимом пути приложения
+        /// </exception>
         public ZipInstaller(string appDir)
         {
             _appDir = appDir;
             _backupDir = Path.Combine(_appDir, "backup");
         }
 
+        /// <summary>
+        /// Выполняет установку обновления из ZIP-файла
+        /// </summary>
+        /// <param name="updateFilePath">Путь к архиву обновления</param>
+        /// <param name="progress">Объект для отслеживания прогресса (0.0-1.0)</param>
+        /// <param name="cancellationToken">Токен отмены операции</param>
+        /// <exception cref="InvalidDataException">
+        /// Некорректный формат ZIP-архива
+        /// </exception>
+        /// <exception cref="UnauthorizedAccessException">
+        /// Отсутствуют права на запись в целевую директорию
+        /// </exception>
         public async Task InstallAsync(string updateFilePath, IProgress<double> progress, CancellationToken cancellationToken = default)
         {
             try
