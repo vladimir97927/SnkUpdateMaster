6904b4079f3ee2d4a9a742db0c709528484c8d15
Vladimir <vladimir97927@gmail.com>
2025-04-20 15:56:54 +0700

    Add ICurrentVersionManager doc

--- src/SnkUpdateMaster.Core/VersionManager/ICurrentVersionManager.cs
+++ src/SnkUpdateMaster.Core/VersionManager/ICurrentVersionManager.cs
@@ -1,9 +1,38 @@
 ﻿namespace SnkUpdateMaster.Core.VersionManager
 {
+    /// <summary>
+    /// Определяет интерфейс для управления текущей версией приложения
+    /// </summary>
+    /// <remarks>
+    /// Интерфейс предоставляет абстракцию для работы с версией приложения:
+    /// <list type="bullet">
+    /// <item><description>Чтение текущей установленной версии</description></item>
+    /// <item><description>Обновление информации о версии после успешной установки</description></item>
+    /// </list>
+    /// Реализации могут использовать различные хранилища:
+    /// <list type="bullet">
+    /// <item><description>Файл конфигурации</description></item>
+    /// <item><description>Метаданные сборки</description></item>
+    /// </list>
+    /// </remarks>
     public interface ICurrentVersionManager
     {
+        /// <summary>
+        /// Асинхронно получает текущую версию приложения
+        /// </summary>
+        /// <returns>
+        /// При успешном выполнении содержит:
+        /// <para>
+        /// <see cref="Version"/> - текущая версия приложения<br/>
+        /// <see langword="null"/> - если версия не определена
+        /// </para>
+        /// </returns>
         Task<Version?> GetCurrentVersionAsync();
 
+        /// <summary>
+        /// Асинхронно обновляет информацию о текущей версии приложения
+        /// </summary>
+        /// <param name="version">Новая версия приложения</param>
         Task UpdateCurrentVersionAsync(Version version);
     }
 }
