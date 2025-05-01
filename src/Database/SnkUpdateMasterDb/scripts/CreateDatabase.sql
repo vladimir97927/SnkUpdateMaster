0ac545e9cebdf097e29bcf06f0967bae07a83c04
Vladimir <vladimir97927@gmail.com>
2025-05-01 23:30:53 +0700

    Add create database script

---
+++ src/Database/SnkUpdateMasterDb/scripts/CreateDatabase.sql
@@ -0,0 +1,12 @@
+﻿USE [master];
+GO
+
+IF (DB_ID(N'$(DatabaseName)') IS NOT NULL) 
+BEGIN
+    ALTER DATABASE [$(DatabaseName)]
+    SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE [$(DatabaseName)];
+END
+GO
+
+CREATE DATABASE [$(DatabaseName)]
