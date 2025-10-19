a9b5c99cca09c17f4bf4dbc5e0fee8ec689b9879
Vladimir <vladimir97927@gmail.com>
2025-10-19 17:04:30 +0700

    Add tests project and try to create builders

---
+++ src/SnkUpdateMaster.Core/Files/JsonUpdateInfoFileParser.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace SnkUpdateMaster.Core.Files
+{
+    public class JsonUpdateInfoFileParser : IUpdateInfoFileParser
+    {
+        public UpdateInfo Parse(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"File not found at path {filePath}");
+            }
+            
+            try
+            {
+                var fileBytes = File.ReadAllBytes(filePath);
+                return Parse(fileBytes);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public UpdateInfo Parse(byte[] fileBytes)
+        {
+            var jsonString = Encoding.UTF8.GetString(fileBytes);
+            var updateInfo = JsonSerializer.Deserialize<UpdateInfo>(jsonString);
+            return updateInfo == null ? throw new Exception("Cant't parse file") : updateInfo;
+        }
+    }
+}
---
+++ src/SnkUpdateMaster.Ftp/Configuration/UpdateManagerBuilderFtpExtensions.cs
@@ -0,0 +1,20 @@
+﻿using SnkUpdateMaster.Core;
+
+namespace SnkUpdateMaster.Ftp.Configuration
+{
+    public static class UpdateManagerBuilderFtpExtensions
+    {
+        public static UpdateManagerBuilder WithFtpUpdateProvider(
+            this UpdateManagerBuilder builder,
+            string host,
+            string username,
+            string password,
+            string downloadsDir,
+            int port = 21)
+        {
+            var ftpClientFactory = new AsyncFtpClientFactory(host, username, password, port);
+
+            
+        }
+    }
+}
