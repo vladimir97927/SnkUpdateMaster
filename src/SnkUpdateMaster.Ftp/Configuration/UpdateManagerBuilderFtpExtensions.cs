552684187e27c3c5a04ef8780060e208b8cdada3
Vladimir <vladimir97927@gmail.com>
2025-11-09 16:33:17 +0700

    Try modify ftp update manager builder

--- src/SnkUpdateMaster.Ftp/Configuration/UpdateManagerBuilderFtpExtensions.cs
+++ src/SnkUpdateMaster.Ftp/Configuration/UpdateManagerBuilderFtpExtensions.cs
@@ -1,20 +1,29 @@
 ﻿using SnkUpdateMaster.Core;
+using SnkUpdateMaster.Core.Files;
 
 namespace SnkUpdateMaster.Ftp.Configuration
 {
     public static class UpdateManagerBuilderFtpExtensions
     {
+        public static UpdateManagerBuilder WithFtpUpdateSource(
+            this UpdateManagerBuilder builder,
+            )
+        {
+
+        }
+
         public static UpdateManagerBuilder WithFtpUpdateProvider(
             this UpdateManagerBuilder builder,
+            IUpdateInfoFileParser updateInfoFileParser,
             string host,
             string username,
             string password,
+            string updateFileInfoPath,
             string downloadsDir,
             int port = 21)
         {
             var ftpClientFactory = new AsyncFtpClientFactory(host, username, password, port);
-
-            
+            var updateSource = new FtpUpdateSource(ftpClientFactory, updateInfoFileParser, updateFileInfoPath);
         }
     }
 }
