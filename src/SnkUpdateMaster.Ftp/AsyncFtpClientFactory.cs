72b81b1104488c31ec5586c93b7724b8e13f445d
Vladimir <vladimir97927@gmail.com>
2025-11-29 22:32:35 +0700

    Add docs to methods

--- src/SnkUpdateMaster.Ftp/AsyncFtpClientFactory.cs
+++ src/SnkUpdateMaster.Ftp/AsyncFtpClientFactory.cs
@@ -2,6 +2,16 @@
 
 namespace SnkUpdateMaster.Ftp
 {
+    /// <summary>
+    /// Фабрика для создания и управления асинхронным FTP-подключением.
+    /// </summary>
+    /// <remarks>Эта фабрика гарантирует повторное использование одного экземпляра FTP-клиента, пока он
+    /// остаётся подключённым. Если клиент отключён, новое соединение будет установлено при вызове <see
+    /// cref="GetConnectClientAsync"/>. Фабрика предназначена для управления жизненным циклом FTP-клиента.</remarks>
+    /// <param name="host">Имя хоста</param>
+    /// <param name="username">Имя пользователя</param>
+    /// <param name="password">Пароль</param>
+    /// <param name="port">Порт (по умолчанию 21)</param>
     public class AsyncFtpClientFactory(
         string host,
         string username,
@@ -26,6 +36,13 @@ namespace SnkUpdateMaster.Ftp
             }
         }
 
+        /// <summary>
+        /// Возвращает экземпляр FTP-клиента, подключаясь к серверу при необходимости.
+        /// </summary>
+        /// <remarks>Если клиент ещё не подключен, создаётся новый экземпляр <see
+        /// cref="AsyncFtpClient"/>, который подключается к серверу, используя предоставленные учётные данные и информацию о хосте.</remarks>
+        /// <param name="cancellationToken">Токен для отслеживания запросов на отмену.</param>
+        /// <returns>Экземпляр <see cref="IAsyncFtpClient"/>, подключенный к FTP-серверу.</returns>
         public async Task<IAsyncFtpClient> GetConnectClientAsync(CancellationToken cancellationToken = default)
         {
             if (_client == null || !_client.IsConnected)
