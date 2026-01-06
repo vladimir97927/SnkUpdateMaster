9c4a415f4ca83b57c0bd3f56ce29313cfc279518
Vladimir <vladimir97927@gmail.com>
2026-01-06 22:58:38 +0700

    Implement new dependency resolve logic

---
+++ src/SnkUpdateMaster.Core/Common/DependencyDescriptor.cs
@@ -0,0 +1,13 @@
+﻿namespace SnkUpdateMaster.Core.Common
+{
+    internal class DependencyDescriptor
+    {
+        public Type? DependencyType { get; set; }
+
+        public Type? ImplementationType { get; set; }
+
+        public object? Instance { get; set; }
+
+        public Func<IDependencyContainer, object>? Factory { get; set; }
+    }
+}
---
+++ src/SnkUpdateMaster.Core/Common/IDependencyContainer.cs
@@ -0,0 +1,13 @@
+﻿namespace SnkUpdateMaster.Core.Common
+{
+    internal interface IDependencyContainer
+    {
+        void RegisterFactory<T>(Func<IDependencyContainer, T> factory);
+
+        void RegisterInstance<T>(T instance);
+
+        T? GetDependency<T>();
+
+        T GetRequiredDependency<T>();
+    }
+}
