bb0ee96f24c6ec2c6ec2ed9abea376de91dee902
Vladimir <vladimir97927@gmail.com>
2026-01-08 16:27:29 +0700

    Edit test

--- tests/SnkUpateMaster.Core.IntegrationTests/IntegrityTests.cs
+++ tests/SnkUpateMaster.Core.IntegrationTests/IntegrityTests.cs
@@ -6,16 +6,32 @@ namespace SnkUpateMaster.Core.IntegrationTests
     [TestFixture]
     internal class IntegrityTests : TestBase
     {
+        private string _zipUpdatePath = string.Empty;
+
+        [SetUp]
+        public new void BeforeEachTest()
+        {
+            _zipUpdatePath = ZipUpdateHelper.CreateMockZipUpdate();
+        }
+
+        [TearDown]
+        public new void AfterEachTest()
+        {
+            if (File.Exists(_zipUpdatePath))
+            {
+                File.Delete(_zipUpdatePath);
+            }
+        }
+
         [Test]
         public void Sha256IntegrityTest()
         {
             var shaIntegrityProvider = new ShaIntegrityProvider();
             var shaIntegrityVerifier = new ShaIntegrityVerifier();
-            var filePath = @"SeedWork\TestApp.zip";
 
-            var checkSum = shaIntegrityProvider.ComputeFileChecksum(filePath);
+            var checkSum = shaIntegrityProvider.ComputeFileChecksum(_zipUpdatePath);
 
-            var isVerified = shaIntegrityVerifier.VerifyFile(filePath, checkSum);
+            var isVerified = shaIntegrityVerifier.VerifyFile(_zipUpdatePath, checkSum);
 
             Assert.That(isVerified, Is.True);
         }
