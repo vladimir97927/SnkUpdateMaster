85e3c0e22a6d74ccd628f12db54a75cad7e4fa46
Vladimir <vladimir97927@gmail.com>
2026-01-09 16:00:16 +0700

    Edit HttpUpdateInfoProvider

--- src/SnkUpdateMaster.Http/HttpUpdateInfoProvider.cs
+++ src/SnkUpdateMaster.Http/HttpUpdateInfoProvider.cs
@@ -1,10 +1,25 @@
-﻿using SnkUpdateMaster.Core;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using SnkUpdateMaster.Core;
+using SnkUpdateMaster.Core.Parser;
 using SnkUpdateMaster.Core.UpdateSource;
 
 namespace SnkUpdateMaster.Http
 {
-    public sealed class HttpUpdateInfoProvider : IUpdateInfoProvider
+    public sealed class HttpUpdateInfoProvider(
+        IHttpClientFactory httpClientFactory,
+        IUpdateInfoParser updateInfoParser,
+        string updateInfoUrl,
+        ILogger<HttpUpdateInfoProvider>? logger = null) : IUpdateInfoProvider
     {
+        private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
+
+        private readonly IUpdateInfoParser _updateInfoParser = updateInfoParser;
+
+        private readonly string _updateInfoUrl = updateInfoUrl;
+
+        private readonly ILogger<HttpUpdateInfoProvider> _logger = logger ?? NullLogger<HttpUpdateInfoProvider>.Instance;
+
         public Task<UpdateInfo?> GetLastUpdatesAsync(CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
