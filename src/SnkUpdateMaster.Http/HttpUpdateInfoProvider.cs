bd5debcd2c0181dd97645cd57eb32f7965aec8db
Vladimir <vladimir97927@gmail.com>
2026-01-09 11:22:43 +0700

    Add http module

---
+++ src/SnkUpdateMaster.Http/HttpUpdateInfoProvider.cs
@@ -0,0 +1,13 @@
+﻿using SnkUpdateMaster.Core;
+using SnkUpdateMaster.Core.UpdateSource;
+
+namespace SnkUpdateMaster.Http
+{
+    public sealed class HttpUpdateInfoProvider : IUpdateInfoProvider
+    {
+        public Task<UpdateInfo?> GetLastUpdatesAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
